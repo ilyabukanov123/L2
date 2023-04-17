5ff809deaeb346991b84d0eeb147e30269284766
ilyabukanov123 <i.d.bukanov@mpt.ru>
2023-04-17 18:08:15 +0300

    Реализован паттерн состояние

--- pattern/08_state.go
+++ pattern/08_state.go
@@ -1,7 +1,53 @@
 package pattern
 
+import (
+	"fmt"
+)
+
 /*
 	Реализовать паттерн «состояние».
 Объяснить применимость паттерна, его плюсы и минусы, а также реальные примеры использования данного примера на практике.
 	https://en.wikipedia.org/wiki/State_pattern
+
+Данный шаблон используется для того, чтобы создавать объект который может менять свое поведение в зависимости от внутреннего состояния
+
+Позволяет избегать многочисленных условий if
+
+State – все состояния который может иметь данный объект
+Context – текущее состояние объекта
+
+cфетовор может гореть 3 цветами
 */
+
+// Интерфейс светофора
+type svetoforInterface interface {
+	ToBur() // светофор может гореть
+}
+
+// Типы состояния
+type RedColor struct{}
+type GreenColor struct{}
+type YellowCollor struct{}
+
+// Реализация интерфейса состояниями
+func (RedColor) ToBur() {
+	fmt.Println("Светофор горит красным цветом")
+}
+
+func (GreenColor) ToBur() {
+	fmt.Println("Светофор горит зеленым цветом")
+}
+
+func (YellowCollor) ToBur() {
+	fmt.Println("Светофор горит зеленым цветом")
+}
+
+// Структура с полем состояния
+type svetoforStruct struct {
+	state svetoforInterface
+}
+
+// func main() {
+// 	svetofor := svetoforStruct{RedColor{}}
+// 	svetofor.state.ToBur()
+// }
