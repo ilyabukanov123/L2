5e9c3bb49473bb5f87536ded2da080734ac21977
ilyabukanov123 <i.d.bukanov@mpt.ru>
2023-04-17 22:04:52 +0300

    Реализован паттерн посетитель

--- pattern/03_visitor.go
+++ pattern/03_visitor.go
@@ -1,7 +1,52 @@
 package pattern
 
+import "fmt"
+
 /*
 	Реализовать паттерн «посетитель».
 Объяснить применимость паттерна, его плюсы и минусы, а также реальные примеры использования данного примера на практике.
 	https://en.wikipedia.org/wiki/Visitor_pattern
+
+	// Расширение функционала структуры без изменения самой структуры
 */
+
+type Car interface {
+	accept(visitor)
+}
+
+type Tesla struct{}
+type Bmw struct{}
+type Audi struct{}
+
+func (Tesla) accept(v visitor) {
+	v.serviceTesla()
+
+}
+
+func (Bmw) accept(v visitor) {
+	v.serviceBmw()
+}
+
+func (Audi) accept(v visitor) {
+	v.serviceAuid()
+}
+
+type visitor interface {
+	serviceTesla()
+	serviceBmw()
+	serviceAuid()
+}
+
+type mechanic struct{}
+
+func (mechanic) serviceTesla() {
+	fmt.Println("Обслуживание Tesla")
+}
+
+func (mechanic) serviceBmw() {
+	fmt.Println("Обслуживание BMW")
+}
+
+func (mechanic) serviceAudi() {
+	fmt.Println("Обслуживание Audi")
+}
